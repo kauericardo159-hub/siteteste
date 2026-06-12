b5c5e1f375a4b439939c7c3b39bbafde1a80b573
Kaue The Protogen <kauericardo159@gmail.com>
2026-06-12 15:45:16 -0300

    Delete js directory

--- js/supabase.js
+++
@@ -1,7 +0,0 @@
-// Substitua pelas credenciais reais que você encontra no painel do seu Supabase
-// Vá em Project Settings -> API para pegar a URL e a Anon Key
-const SUPABASE_URL = "https://bdidmnhjololaamnxrlc.supabase.co/rest/v1/";
-const SUPABASE_KEY = "sb_publishable_Qq_4xTnK9jvOqSuvAUi1VQ_XRoyS6Tv";
-
-// Inicializa o cliente do Supabase no navegador
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
